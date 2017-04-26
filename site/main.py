90e444b4d643f3dccaaf6653e717161871c7cc05
justindalzell <justin.d.dalzell@gmail.com>
2017-04-26 02:11:08 -0700

    Update main.py

--- site/main.py
+++ site/main.py
@@ -1,30 +1,24 @@
-# Copyright 2015 webapp2 AUTHORS.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 import webapp2
 
+form="""
+<form method="post" action="/testform">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
 
-NEW_BASE_URL = 'https://webapp2.readthedocs.org/'
-
-
-class RedirectHandler(webapp2.RequestHandler):
-    def get(self, uri):
-        return webapp2.redirect(
-            NEW_BASE_URL + uri,
-            permanent=True)
-
-
-app = webapp2.WSGIApplication([
-    webapp2.Route(r'/<:.*>', RedirectHandler)])
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+    	self.response.headers['Content-Type'] = 'text/html'
+    	self.response.write(form)
+    	
+class TestHandler(webapp2.RequestHandler):
+	def get(self):
+		q = self.request.get("q")
+		self.response.out.write(q)
+		#self.response.headers['Content-Type'] = 'text/html'
+		#self.response.out.write(self.request)
+		
+app = webapp2.WSGIApplication([('/', MainPage),
+								('/testform', TestHandler)],
+								debug=True)
